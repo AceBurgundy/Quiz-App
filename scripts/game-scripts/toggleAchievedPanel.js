3f6191739fbbadc1067528efef1fa7f94e363e96
Sam Adrian <samadriansabalo99@gmail.com>
2023-06-22 06:58:26 +0800

    Update toggleAchievedPanel.js

--- scripts/game-scripts/toggleAchievedPanel.js
+++ scripts/game-scripts/toggleAchievedPanel.js
@@ -11,86 +11,51 @@ export default function showPassedPanel(congratulations = false) {
     const panelText = document.getElementById("achieved-panel__text");
     const stopButton = document.getElementById("achieved-panel__buttons-stop");
     const nextButton = document.getElementById("achieved-panel__buttons-next");
-    const scorePlaceholder = document.getElementById("achieved-panel__text-score")
-    
+    const scorePlaceholder = document.getElementById("achieved-panel__text-score");
+
     Global.incrementIndex();
     panel.classList.add("active");
-    panelBackground.classList.add("active")
-    Global.saveData()
+    panelBackground.classList.add("active");
+    Global.saveData();
 
     if (Global.getScore() == Global.getLimit() || congratulations) {
 
-        const won = Global.getScore() === Global.getLimit()
-        const score = won ? "win" : "lose"
-        playSound(score)
+        const won = Global.getScore() === Global.getLimit();
+        const score = won ? "win" : "lose";
+        playSound(score);
 
-        panelText.textContent = won ? `Congratulations!` : "Better Luck Next Time";
+        panelText.textContent = won ? "Congratulations!" : "Better Luck Next Time";
         stopButton.textContent = "Menu";
-        nextButton.style.display = "none"
+        nextButton.style.display = "none";
         scorePlaceholder.textContent = `Score: ${Global.getScore()}/${Global.getLimit()}`;
 
         stopButton.onclick = function() {
-
-            fetch("https://quizeme.pythonanywhere.com/update_score", {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json",
-                },
-                body: JSON.stringify({ name: Global.getPlayerName(), score: Global.getScore() }),
-            })
-            .then(response => response.json())
-            .then(data => {
-                if (data.status === "success") {
-                    redirect("game-panel", "menu.html")
-                    makeToast(data.message);
-                } else {
-                    makeToast(data.message);
-                }
-            })
-
-            scorePlaceholder.textContent = ""
-            panelText.textContent = ""
+            scorePlaceholder.textContent = "";
+            panelText.textContent = "";
             panel.classList.remove("active");
-            panelBackground.classList.remove("active")            
-            nextButton.style.display = "block"
+            panelBackground.classList.remove("active");
+            nextButton.style.display = "block";
         };
 
         return;
 
     } else {
 
-        playSound("wordCorrect")
+        playSound("wordCorrect");
         panelText.textContent = "You got it right!";
         stopButton.textContent = "End";
         nextButton.textContent = "Next";
 
         stopButton.onclick = function() {
-            Global.setCurrentIndex(Global.getScore())
-            Global.saveData()
+            Global.setCurrentIndex(Global.getScore());
+            Global.saveData();
             showPassedPanel(true);
-
-            fetch("https://quizeme.pythonanywhere.com/update_score", {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json",
-                },
-                body: JSON.stringify({ name: Global.getPlayerName(), score: Global.getScore() }),
-            })
-            .then(response => response.json())
-            .then(data => {
-                if (data.status === "success") {
-                    makeToast(data.message);
-                    redirect("game-panel", "menu.html")
-                } else {
-                    makeToast(data.message);
-                }
-            })
         };
 
         nextButton.onclick = function(event) {
 
-            event.preventDefault()
-
+            event.preventDefault();
+            makeToast("Updating Score");
             fetch("https://quizeme.pythonanywhere.com/update_score", {
                 method: "POST",
                 headers: {
@@ -101,21 +66,23 @@ export default function showPassedPanel(congratulations = false) {
             .then(response => response.json())
             .then(data => {
                 if (data.status === "success") {
+                    panel.classList.remove("active");
+                    panelBackground.classList.remove("active");
+                    runGame();
                     makeToast(data.message);
                 } else {
                     makeToast(data.message);
                 }
             })
-            panel.classList.remove("active");
-            panelBackground.classList.remove("active")
-            runGame();
+            .catch(error => makeToast("Cannot update score. Check your internet connection"));
+            
         };
         return;
     }
 }
 
 Game.click("menu-prompt__yes", () => {
-    Global.setCurrentIndex(Global.getScore())
-    Global.saveData()
-    redirect("game-panel", "menu.html")
-})
+    Global.setCurrentIndex(Global.getScore());
+    Global.saveData();
+    redirect("game-panel", "menu.html");
+});
