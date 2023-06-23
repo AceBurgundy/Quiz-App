3f8cf8c976a9272ec2b9815235699f4f1d5b3e73
AceBurgundy <Samadriansabalo99@gmail.com>
2023-06-23 15:19:11 +0800

    Added if online checks and finished delete players fetch.

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,16 +1,19 @@
 import Global from "./global.js";
 import Game from "./helpers/events.js";
 import redirect from "./helpers/redirect.js";
+import makeToast from "./helpers/toast.js";
 
 const element = (element) => document.querySelector(element);
 
-window.onload = () => { 
-  element("#menu-panel").style.transform = "translateY(0%)"
-}
-
-const settingsCover = document.getElementById("background-cover");
+window.onload = () => {
+    element("#menu-panel").style.transform = "translateY(0%)";
+};
 
 const playerName = Global.getPlayerName();
+
+if (playerName == "") {
+    redirect("menu-panel", "name.html")
+}
 
 function menuPanelChild(props) {
     const { title, button, playerName, menuScore, menuScoreLimit } = props;
@@ -63,7 +66,7 @@ const title = element("#menu-panel__game-title");
 fitText(title, 0.37);
 
 const menuButtonsContainer = element("#menu-panel__buttons-container").children;
-const settingsContainer = element("#settings-container").children;
+const settings = element("#settings").children;
 
 function handleOrientationChange() {
     if (window.matchMedia("(orientation: portrait)").matches) {
@@ -72,11 +75,11 @@ function handleOrientationChange() {
         fitText(element("#menu-panel__game-title-description"), 2.5);
         fitText(element("#menu-panel__buttons-start"), 0.7);
         [...menuButtonsContainer].forEach((child) => fitText(child, 1.1));
-        [...settingsContainer].forEach((child) => fitText(child, 0.9));
+        [...settings].forEach((child) => fitText(child, 0.9));
         fitText(element("#menu-panel__score-data"), 2.5);
     } else {
         [...menuButtonsContainer].forEach((child) => fitText(child, 0.9));
-        [...settingsContainer].forEach((child) => fitText(child, 0.9));
+        [...settings].forEach((child) => fitText(child, 1.4));
     }
 }
 
@@ -92,10 +95,25 @@ Global.getScore() === 0
     ? (scoreContainerChildren.style.display = "none")
     : (scoreContainerChildren.style.display = "block");
 
+const backgroundPanel = document.getElementById("background-panel");
+Game.click("x-icon", () => {
+    backgroundPanel.classList.remove("active");
+    element(element("#x-icon").getAttribute("data-clip")).classList.remove(
+        "show"
+    );
+    element("#x-icon").setAttribute("data-clip", "");
+});
+
 //resets data
 Game.click("menu-panel__buttons-reset", () => {
+
+    if (!navigator.onLine) {
+        makeToast("No internet connection")
+        return
+    }
+
     Global.resetData();
-    settingsCover.classList.remove("active");
+    backgroundPanel.classList.remove("active");
     redirect("menu-panel", "name.html");
 });
 
@@ -104,51 +122,105 @@ Game.click("menu-panel__buttons-start", () => {
     redirect("menu-panel", "game.html");
 });
 
-Game.click("x-icon", () => {
-    settingsCover.classList.remove("active");
-});
-
 Game.click("menu-panel__buttons-settings", () => {
-    settingsCover.classList.add("active");
+    backgroundPanel.classList.add("active");
+    backgroundPanel.firstElementChild.setAttribute("data-clip", "#settings");
+    element("#settings").classList.add("show");
 });
 
 Game.click("menu-panel__buttons-quit", () => {
     redirect("menu-panel", "../index.html");
 });
 
-const gameCover = document.getElementById("players-cover")
-const playerContainer = document.getElementById("players")
+const playerContainer = document.getElementById("players");
 
 Game.click("menu-panel__buttons-show-players", () => {
-  gameCover.classList.add("active");
 
-  fetch("https://quizeme.pythonanywhere.com/get_players", {
-                method: "GET"
-            })
-			.then(response => response.json())
-			.then(data => {
+    if (!navigator.onLine) {
+        makeToast("No internet connection")
+        return
+    }
 
-				if (data.status === "success") {
-          data.players.forEach(data => {
-            playerContainer.innerHTML += `
-            <li class="player-item">
-              <p class="player-name">Player: ${data.name}</p> 
-              <p class="player-score">Score: ${data.score}</p>
-            </li>`
-          })
+    backgroundPanel.classList.add("active");
+    backgroundPanel.firstElementChild.setAttribute("data-clip", "#players");
+
+    element("#players").classList.add("show");
+
+    playerContainer.innerHTML += `<p>Loading Players</p>`;
+    fetch("http://127.0.0.1:5000/get_players", {
+        method: "GET",
+    })
+    .then((response) => response.json())
+    .then((data) => {
+        if (data.status === "success") {
+            playerContainer.innerHTML = ``;
+            data.players.forEach((data) => {
+                playerContainer.innerHTML += `
+                  <div class="player-item">
+                    <p class="player-name">Player: ${data.name}</p> 
+                    <p class="player-score">Score: ${data.score}</p>
+                  </div><br>`;
+            });
+        } else {
+            playerContainer.innerHTML = `<p>${data.message}</p>`;
         }
-			})
+    })
+    .catch((error) => {
+        playerContainer.innerHTML = `<p class="error-message">Failed to connect to the server.</p>`;
+    });
 });
 
-Game.click("player-x-icon", () => {
-  gameCover.classList.remove("active");
-  playerContainer.innerHTML = ""
+Game.click("menu-panel__buttons-clear", () => {
+    
+    if (!navigator.onLine) {
+        makeToast("No internet connection")
+        return
+    }
+
+    element("#extra-background-panel").classList.add("active");
+    element("#delete-players-form").classList.add("show");
+});
+
+Game.click("form-close-icon", () => {
+    element("#extra-background-panel").classList.remove("active");
+    element("#delete-players-form").classList.remove("show");
+});
+
+Game.click("delete-players-form__submit", () => {
+
+	const password = element("#password")
+
+	if (password.value.trim() === "") {
+        makeToast("Password is needed")
+        return
+    }
+    
+	fetch("http://127.0.0.1:5000/delete_players", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ password: password.value }),
+    })
+    .then((response) => response.json())
+    .then((data) => {
+        if (data.status === "success") {
+            makeToast(data.message)
+            Global.resetData(false)
+            setTimeout(() => {
+                redirect("menu-panel", "name.html")
+            }, 6000);
+        } else {
+            makeToast(data.message);
+        }
+    });
 });
 
 Game.click("menu-panel__buttons-instructions", () => {
-  document.getElementById("game-instructions-box").classList.add("show")
-})
-
-Game.click("panel-x-icon", () => {
-  document.getElementById("game-instructions-box").classList.add("hide")
-})
+    backgroundPanel.classList.add("active");
+    element("#game-instructions").classList.add("show");
+    backgroundPanel.firstElementChild.setAttribute(
+        "data-clip",
+        "#game-instructions"
+    );
+});
